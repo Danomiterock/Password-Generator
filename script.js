b6686a3451c3919f13b3cc09b8209929e90547cd
Daniel Lucas <danomiterock@yahoo.com>
2021-02-26 14:44:29 -0700

    add uppper case letters and strategy remarks

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+console.log(generateBtn);
 
 // Write password to the #password input
 function writePassword() {
@@ -13,4 +14,23 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Create a array for upper case alpha, lower
+
 var lowerCaseLetters = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+
+var upperCaseLetters = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X","Y", "Z")
+
+function writePassword() {
+  var password = generatePassword ();
+  var passwordText = document.querySelector("#generate");
+}
+
+function generatePassword () {
+  //Prompt User for special characters
+  //Prompt user for lower alphs
+  //Prompt user for upper alpha
+  //Prompt user for number
+  //Prompt user for password length
+  
+  return "Password should go here"
+}
