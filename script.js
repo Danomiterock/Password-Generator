adb55737398f4267077ae6728ea474f145b1bf80
Daniel Lucas <danomiterock@yahoo.com>
2021-02-27 10:24:14 -0700

    add get random character and splice code

--- script.js
+++ script.js
@@ -24,6 +24,9 @@ var specialCharacter = ("!", "@", "#", "$", "%", "^", "&", "*", "<", ">","/", "~
 
 var numericalCharacter = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
 
+//Defines an item as a vlue selected from a specific array
+var item = item[Math.floor(Math.random()*item.length)]
+
 function writePassword() {
   var password = generatePassword ();
   var passwordText = document.querySelector("#generate");
@@ -53,33 +56,33 @@ for(let i = 0; i < i , length; i++) {
 
 //Display password on the screen- is this what the return will do?
 
-# User Story
+// # User Story
 
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
+// ```
+// AS AN employee with access to sensitive data
+// I WANT to randomly generate a password that meets certain criteria
+// SO THAT I can create a strong password that provides greater security
+// ```
 
-## Acceptance Criteria
+// ## Acceptance Criteria
 
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
+// ```
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+// ```
 
 //## Mock-Up
 
